16589b33250038e0a7ec7b16b38716786344f7ff
Chaplinskii <6945053@gmail.com>
2024-05-14 20:41:04 +0300

    написал реализацию программы состоящую из 2 функций

--- Program.cs
+++ Program.cs
@@ -1,1 +1,33 @@
 ﻿
+string[] forming_an_array_of_strings(string[] arr)
+{
+    string[] final_arr = new string[arr.Length];
+    int count =0;
+    for (int i =0; i<arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            final_arr[count]= arr[i];
+            count++;
+        }
+    }
+    return final_arr;
+}
+
+
+void PrintArray(string[] arr)
+{
+    Console.Clear();
+    foreach (var s in arr)
+    {
+        if (s != null)
+            Console.Write("\""+s+"\" ");
+    }
+
+    Console.WriteLine();
+}
+
+string[] array1 = new string[5] { "123", "23", "hello", "world", "res" };
+string[] array2 = new string[4] { "1234", "1567", "-2", "computer science" };
+string[] array3 = new string[3] { "Russia", "Denmark", "Kazan" };
+PrintArray(forming_an_array_of_strings(array1));
